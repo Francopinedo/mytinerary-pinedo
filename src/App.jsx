9ffb140485d5b443951556bb4b621fdb8a8cf7ac
Francopinedo <francopinedo23@gmail.com>
2023-08-01 23:26:06 -0300

    V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='header-top'>
+       
+     <div>
+      <img src="" alt="" />
+     <a href="#">My Tinerary</a>
+     </div>
+     <nav>
+        <a href="#">Home</a>
+      <a href="#">Cities</a>
+      <a href="#">Login</a>
+        </nav>
+      </header>
+
+
+
+      
+      <main className='main'>
+        <section>
+        <h1>Tittle</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi ipsam doloremque, enim 
+        atque optio hic sapiente laudantium earum? Deleniti natus aliquam inventore aut adipisci 
+        ducimus eum possimus ex minima facere.</p>
+
+        </section>
+        <div className='main-2'>
+        <img src="" alt="" />
+        </div>
+      
+      </main>
+      
+        
+        
+      
+
+
+      <footer className='footer' >
+       FOOTER
+      </footer>
     </>
   )
 }
